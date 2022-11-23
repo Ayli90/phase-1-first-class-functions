c5a5c757b77aca398c087b36ba1ec2aa5ae5441e
Amy Li <amyl@buffalo.edu>
2022-11-23 09:44:21 -0500

    commit tree 8be026142c5e19c61ceb6d263923fdb3d97a04a5

---
+++ index.js
@@ -0,0 +1,14 @@
+function morningRoutine(exercise) {
+    let breakfast;
+  
+    if (exercise === liftWeights) {
+      breakfast = "protein bar";
+    } else if (exercise === swimFortyLaps) {
+      breakfast = "kale smoothie";
+    } else {
+      breakfast = "granola";
+    }
+  
+    exerciseRoutine(exercise);
+}
+const receivesAFunction = morningRoutine(liftWeights)
